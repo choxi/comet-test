f3c1c766f7dcce61ee32796c5b18c6173e568491
Roshan Choxi <choxi@users.noreply.github.com>
2017-09-21 22:27:55 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,6 +2,11 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+/**
+ *  @name App
+ *  @example
+ *  <App />
+ */
 class App extends Component {
   render() {
     return (
